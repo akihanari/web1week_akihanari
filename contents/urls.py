260f0d735cf9c33368d2d3638cf05318d0657b86
akihanari <akihanari@gmail.com>
2020-05-23 08:32:49 +0900

    20200523_urls

--- contents/urls.py
+++ contents/urls.py
@@ -3,16 +3,16 @@ from django.urls import path
 from django.conf.urls import include, url
 # from django.contrib.auth import views
 import django.contrib.auth.views
-from .views import login_func, logout_func
+from .views import login_func, logout_func, top_page
 from . import views
 
 urlpatterns = [
     # path('', include('social_django.urls', namespace = 'social')),
     path('', include('social_django.urls')),
     # path('', views.index_func, name="index"),
-    path('', views.login_func, name="login"),
-    path('logout/', views.logout_func, name="logout"),
+    path('', login_func, name="login"),
+    path('logout/', logout_func, name="logout"),
     # path('login/', views.login_func, name="login"),
-    path('top/', views.top_page, name="top_page"),
+    path('top/', top_page, name="top_page"),
 
 ]
