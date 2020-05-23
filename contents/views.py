801af593a4cdd4e943b75fd591f84195f01bcc01
akihanari <akihanari@gmail.com>
2020-05-24 06:30:53 +0900

    20200524_views

--- contents/views.py
+++ contents/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.contrib.auth.decorators import login_required
 from social_django.models import UserSocialAuth
-
+import tweepy
 
 
 def login_func(request):
@@ -22,25 +22,25 @@ def top_page(request):
 
     social_account = UserSocialAuth.objects.get(user_id=request.user.id)
     user_oauth_token = social_account.extra_data['access_token']['oauth_token']
-    user_oauth_token_sercret = social_account.extra_data['access_token']['oauth_token_secret']
+    user_oauth_token_secret = social_account.extra_data['access_token']['oauth_token_secret']
     # screen_name = social_account['screen_name']
 
 
 
     # url = 'https://api.twitter.com/1.1/users/show.json' + '?user_id=' + request.user.id + '&include_entities=true'
+    #
+    # params = {}
 
-    params = {}
-
-    url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+    # url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
 
     # OAuth で GET
-    twitter = OAuth1Session(SOCIAL_AUTH_TWITTER_KEY, SOCIAL_AUTH_TWITTER_SECRET, user_oauth_token, user_oauth_token_sercret)
-
-    req = twitter.get(url, params = params)
-
-    if req.status_code == 200:
-        # レスポンスはJSON形式なので parse する
-        timeline = json.loads(req.text)
+    # twitter = OAuth1Session(SOCIAL_AUTH_TWITTER_KEY, SOCIAL_AUTH_TWITTER_SECRET, user_oauth_token, user_oauth_token_secret)
+    #
+    # req = twitter.get(url, params = params)
+    #
+    # if req.status_code == 200:
+    #     # レスポンスはJSON形式なので parse する
+    #     timeline = json.loads(req.text)
         # 各ツイートの本文を表示
         # for tweet in timeline:
         #     # print(tweet["text"])
@@ -48,6 +48,15 @@ def top_page(request):
     # else:
         # エラーの場合
         # print ("Error: %d" % req.status_code)
+
+
+    # OAuth認証
+    auth = tweepy.OAuthHandler(SOCIAL_AUTH_TWITTER_KEY, SOCIAL_AUTH_TWITTER_SECRET)
+    auth.set_access_token(user_oauth_token, user_oauth_token_secret)
+    api = tweepy.API(auth)
+
+    # Timelineメソッド、stringで返却される
+    timeline = api.home_timeline()
 
     social_dic = {'social_account': social_account ,'user_oauth_token': user_oauth_token, 'user_oauth_token_sercret': user_oauth_token_sercret, 'timeline': timeline}
 
