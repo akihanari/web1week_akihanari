f4d169eebfd1a553afe1f6ca7b553d623a23d98b
akihanari <akihanari@gmail.com>
2020-05-24 05:43:45 +0900

    20200524_views

--- contents/views.py
+++ contents/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from requests_oauthlib import OAuth1Session
+import json
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.contrib.auth.decorators import login_required
 from social_django.models import UserSocialAuth
+
 
 
 def login_func(request):
@@ -15,18 +17,42 @@ def login_func(request):
 def logout_func(request):
     return render(request, "logout.html")
 
-
 @login_required
 def top_page(request):
-    user_api = 'https://api.twitter.com/1.1/users/show.json'
 
     social_account = UserSocialAuth.objects.get(user_id=request.user.id)
     user_oauth_token = social_account.extra_data['access_token']['oauth_token']
     user_oauth_token_sercret = social_account.extra_data['access_token']['oauth_token_secret']
     # screen_name = social_account['screen_name']
-    screen_name = social_account.access_token.screen_name
 
-    social_dic = {'social_account': social_account ,'user_oauth_token': user_oauth_token, 'user_oauth_token_sercret': user_oauth_token_sercret , 'screen_name':screen_name}
+    social_dic = {'social_account': social_account ,'user_oauth_token': user_oauth_token, 'user_oauth_token_sercret': user_oauth_token_sercret}
+
+    url = 'https://api.twitter.com/1.1/users/show.json' + '?user_id=' + request.user.id + '&include_entities=true'
+
+    oath_key_dict = {
+        "consumer_key": SOCIAL_AUTH_TWITTER_KEY,
+        "consumer_secret": SOCIAL_AUTH_TWITTER_SECRET,
+        "access_token": user_oauth_token,
+        "access_token_secret": "user_oauth_token_sercret
+    }
+
+    params = {}
+
+    # OAuth で GET
+    twitter = OAuth1Session(CK, CS, AT, AS)
+    req = twitter.get(url, params = params)
+
+    if req.status_code == 200:
+        # レスポンスはJSON形式なので parse する
+        timeline = json.loads(req.text)
+        # 各ツイートの本文を表示
+        for tweet in timeline:
+            print(tweet["text"])
+
+    else:
+        # エラーの場合
+        print ("Error: %d" % req.status_code)
+
     return render(request,'top.html', social_dic)
 
     # form = TweetForm
