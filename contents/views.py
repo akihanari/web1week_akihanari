a32c225fad7509e2342d98bec97f82cb984c1c01
akihanari <akihanari@gmail.com>
2020-05-23 09:21:17 +0900

    20200523_views

--- contents/views.py
+++ contents/views.py
@@ -24,6 +24,17 @@ def top_page(request):
     user = UserSocialAuth.objects.get(user_id=request.user.id)
     pageDic = {
         'hoge': 'fuga',
-        'user': user
+        'user': user,
+        'screen_name': user.access_token.screen_name,
     }
     return render(request, 'top.html', pageDic)
+
+
+# UserId:{{ user.access_token.user_id }}</p>
+# <p>OAuthTokenSecret:{{ user.access_token.oauth_token_secret }}</p>
+# <p>OAuthToken:{{ user.access_token.oauth_token }}</p>
+# <p>スクリーンネーム:{{ user.access_token.screen_name }}</p>
+# <p>ツイートの合計数:{{ user.access_token.statuses_count }}</p>
+# <p>ウェブサイト:{{ user.access_token.url }}</p>
+# <p>フォロワー数:{{ user.access_token.followers_count }}</p>
+# <p>最新ツイート:{{ user.access_token.status{text} }}</p>
